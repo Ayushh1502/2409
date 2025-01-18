87684b14c9416ff55266a829f0aa037623329db2
jasminekeshari <jasminekeshari2@gmail.com>
2025-01-18 23:18:12 +0530

    home page

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,18 +1,24 @@
-let loginForm = document.querySelector('.login-form');
+// let loginForm = document.querySelector('.login-form');
 
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
+// document.querySelector('#login-btn').onclick = () =>{
+//     loginForm.classList.toggle('active');
+//     navbar.classList.remove('active');
+// }
 
-let navbar = document.querySelector('.navbar');
+// let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
-}
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+//     loginForm.classList.remove('active');
+// }
 
-window.onscroll = () =>{
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
-}
+// window.onscroll = () =>{
+//     loginForm.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
+let login= document.getElementById("start");
+login.addEventListener("click", ()=> {
+    window.location="login.html";
+})
+
